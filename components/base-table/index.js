b49d42fbcfd8aa71c63757f7e19ea26f6bccfaaa
dantsub <dany.mrtinez@gmail.com>
2022-10-16 21:10:21 -0500

    Add mock table

--- components/base-table/index.js
+++ components/base-table/index.js
@@ -21,6 +21,10 @@ class BaseTable extends HTMLElement {
       th.textContent = content === "messagetext" ? "Message" : content;
       tr.appendChild(th);
     });
+    const actionTh = document.createElement("th");
+    actionTh.classList.add("px-2", "py-3");
+    actionTh.textContent = "Acciones";
+    tr.appendChild(actionTh);
     thead.appendChild(tr);
     return thead;
   };
@@ -30,6 +34,20 @@ class BaseTable extends HTMLElement {
     td.classList.add("p-2");
     td.textContent = content;
     return td;
+  };
+
+  createLink = (href, children) => {
+    const a = document.createElement("a");
+    a.setAttribute("href", href);
+    a.appendChild(children);
+    return a;
+  }
+
+  createIcon = (icon, className) => {
+    const c = className || "";
+    const i = document.createElement("i");
+    i.setAttribute("class", `text-slate-500 ${c} hover:text-slate-700 fi fi-rr-${icon}`);
+    return i;
   };
 
   createBody = () => {
@@ -46,6 +64,15 @@ class BaseTable extends HTMLElement {
       values.forEach((value) => {
         tr.appendChild(this.addItems(value));
       });
+      const icon1 = this.createLink("./show.html", this.createIcon("eye"));
+      const icon2 = this.createLink("./edit.html", this.createIcon("pencil", "px-4"));
+      const icon3 = this.createLink("./delete.html", this.createIcon("trash"));
+      const actionsTd = document.createElement("td");
+      actionsTd.setAttribute("class", "p-2 text-center");
+      actionsTd.appendChild(icon1);
+      actionsTd.appendChild(icon2);
+      actionsTd.appendChild(icon3);
+      tr.appendChild(actionsTd);
       tbody.appendChild(tr);
     });
     return tbody;
